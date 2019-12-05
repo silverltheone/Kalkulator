8784ffcae07be71f08b7778da111f4562d796870
silverltheone <adeksiwy@gmail.com>
2019-12-05 18:51:19 +0100

    First Commit

---
+++ src/main/java/com/kalkulator/ProjectKalkulatorApplication.java
@@ -0,0 +1,13 @@
+package com.kalkulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKalkulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectKalkulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kalkulator/ProjectKalkulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalkulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKalkulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
